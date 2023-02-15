d3599752dd00365081875d153c00149c033fe628
hectorwilder93 <cocorotero@hotmail.com>
2023-02-15 16:44:21 -0500

    Registro e historia de los pacientes en un consultorio médico

---
+++ js/index.js
@@ -0,0 +1,11 @@
+
+document.getElementById('boton-perfil').addEventListener("click", function(){
+  
+})
+class persona = {
+    constructor(nombre,ciudad,entidad){
+        this.nombre = nombre;
+        this.ciudad = ciudad;
+        this.entidad = entidad;
+    }
+masa()
